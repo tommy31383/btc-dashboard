381363ce721bf7c76a1b03ee173bb1425a3cd42d
tommy31383 <tuantommy83@gmail.com>
2026-07-04 12:13:14 +0700

    deploy: v4.14.1 web

--- App.tsx
+++ App.tsx
@@ -64,7 +64,7 @@ const CACHE_KEYS = [
   "@btc_backtest_candles",
   "@btc_config_source_by_tf",
 ];
-const APP_VERSION = "4.13.3";
+const APP_VERSION = "4.14.1";
 const BUILD_DATE = "2026-07-04";
 
 /**
